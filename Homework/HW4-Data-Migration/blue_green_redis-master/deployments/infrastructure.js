6f1210778198969452c9365a0901c9b42992e3f3
FeifeiWang7 <fwang12@ncsu.edu>
2015-04-17 19:19:11 -0400

    :D

--- Homework/HW4-Data-Migration/blue_green_redis-master/deployments/infrastructure.js
+++ Homework/HW4-Data-Migration/blue_green_redis-master/deployments/infrastructure.js
@@ -1,7 +1,7 @@
-#!/bin/sh
+/* Learned from JesseXu*/
 var http = require('http');
 var httpProxy = require('http-proxy');
-var exec = require('child_process').exec;
+var exec = require('child_process').exec; // 
 var request = require("request");
 var redis = require('redis')
 
@@ -18,55 +18,53 @@ var infrastructure =
 		greenclient.del("myimg");
 		blueclient.del("switch");
 		greenclient.del("switch");
-        var options = {};
-        var proxy = httpProxy.createProxyServer(options);
-
-        var server = http.createServer(function (req, res) {
-            proxy.web(req, res, {target: TARGET});
-        });
-        server.listen(8181);
+        	var options = {};
+        	var proxy = httpProxy.createProxyServer(options);
+		var server = http.createServer(function (req, res) {
+			proxy.web(req, res, {target: TARGET});
+        	});
+        	server.listen(8181);
 		
-        exec('forever start -w --watchDirectory ../blue-www ../blue-www/main.js 9090');
-        console.log("blue slice");
-
-        exec('forever start -w --watchDirectory ../green-www ../green-www/main.js 5060');
-        console.log("green slice");
+        	exec('forever start -w --watchDirectory ../blue-www ../blue-www/main.js 9090');
+        	console.log("blue slice");
+        	exec('forever start -w --watchDirectory ../green-www ../green-www/main.js 5060');
+        	console.log("green slice");
 
 		function migrate(target){
 			if(target==GREEN){
-                blueclient.llen("myimg",function(error,num){
-                    console.log("Migrating to Green Begin!! Copying "+num+" data")
-                    if(num!=0){
-                        (blueclient.lrange("myimg",0,-1,function(err,items){
-                            if(err) throw err;
-                            items.forEach(function(item){
-                                greenclient.lpush('myimg',item);
-                            })
-                        }))
-                    }
-                });
+       		        	blueclient.llen("myimg",function(error,num){
+        	        		console.log("Migrating to Green Begin!! Copying "+num+" data")
+                	        	if(num!=0){
+                        			(blueclient.lrange("myimg",0,-1,function(err,items){
+                            				if(err) throw err;
+                            				items.forEach(function(item){
+                                			greenclient.lpush('myimg',item);
+                        				})
+						}))
+					}
+				});
 			}
 			else{
-                greenclient.llen("myimg",function(error,num){
-                    console.log("Migrating to Blue Begin!! Copying "+num+" data")
-                    if(num!=0){
-                        (greenclient.lrange("myimg",0,-1,function(err,items){
-                            if(err) throw err;
-                            items.forEach(function(item){
-                                blueclient.lpush('myimg',item);
-                            })
-                        }))
-                    }
-                });
+                		greenclient.llen("myimg",function(error,num){
+                    			console.log("Migrating to Blue Begin!! Copying "+num+" data")
+                    			if(num!=0){
+                        			(greenclient.lrange("myimg",0,-1,function(err,items){
+                            				if(err) throw err;
+                            				items.forEach(function(item){
+                                				blueclient.lpush('myimg',item);
+                            				})
+                        			}))
+                    			}
+                		});
 			}
 		}
-        function trigger(){
+        	function trigger(){
 			blueclient.get("switch",function(err,value){
 				if(value=="1"){
-                    TARGET=GREEN;
-                    server = http.createServer(function (req, res) {
-                        proxy.web(req, res, {target: TARGET});
-                    });
+					TARGET=GREEN;
+                    			server = http.createServer(function (req, res) {
+                        			proxy.web(req, res, {target: TARGET});
+                    			});
 					console.log("switch to green "+GREEN);
 					blueclient.del("switch");
 					if(mirror==false) migrate(GREEN);
@@ -74,85 +72,76 @@ var infrastructure =
 			})
 			greenclient.get("switch",function(err,value){
 				if(value=="1"){
-                    TARGET=BLUE;
-                    server = http.createServer(function (req, res) {
-                        proxy.web(req, res, {target: TARGET});
-                    });
+                    			TARGET=BLUE;
+                    			server = http.createServer(function (req, res) {
+                        			proxy.web(req, res, {target: TARGET});
+                    			});
 					console.log("switch to blue "+BLUE);
 					greenclient.del("switch");
 					if(mirror==false) migrate(BLUE);
 				}
 			})
-        }
-        var init_green=0;
-        var init_blue=0;
-        blueclient.llen('myimg',function(err,num){
-                init_blue=num;
-            }
-        )
-        greenclient.llen('myimg',function(err,num){
-                init_green=num;
-            }
-        )
+        	}
+        	var init_green=0;
+        	var init_blue=0;
+        	blueclient.llen('myimg',function(err,num){
+                	init_blue=num;
+            	})
+        	greenclient.llen('myimg',function(err,num){
+                	init_green=num;
+            	})
 		blue_flag=0;
 		green_flag=0;
 		add=0;
-        function dup(){
-            blueclient.llen('myimg',function(err,num){
-                    if(num>init_blue){
-                        blue_flag=1;
-                        init_blue=num;
-						add=num-init_blue;
-                    }
-                }
-            );
-            greenclient.llen('myimg',function(err,num){
-                    if(num>init_green){
-                        green_flag=1;
-                        init_green=num;
-						add=num-init_green;
-                    }
-                }
-            );
-            if(blue_flag==1 && green_flag==0){
-                blueclient.lrange("myimg",0,add,function(err,items){
-                    console.log("in blue")
-                    if(err) throw err;
-                    items.forEach(function(item){
-                        greenclient.lpush('myimg',item);
-						init_green++;
-                    })
-                })
-				blue_flag=0;
-            }
-            if(green_flag==1 && blue_flag==0){
-                console.log("in green");
-                greenclient.lrange("myimg",0,add,function(err,items){
-                    if(err) throw err;
-                    items.forEach(function(item){
-                        blueclient.lpush('myimg',item);
-						init_blue++;
-                    })
-                })
-				green_flag=0;
-            }
+        	function dup(){
+            		blueclient.llen('myimg',function(err,num){
+                    		if(num>init_blue){
+                        		blue_flag=1;
+                        		init_blue=num;
+					add=num-init_blue;
+                    		}
+                	});
+            	greenclient.llen('myimg',function(err,num){
+			if(num>init_green){
+                        	green_flag=1;
+                        	init_green=num;
+				add=num-init_green;
+                    	}
+                });
+		if(blue_flag==1 && green_flag==0){
+                	blueclient.lrange("myimg",0,add,function(err,items){
+                    		console.log("in blue")
+                    		if(err) throw err;
+                    		items.forEach(function(item){
+                        		greenclient.lpush('myimg',item);
+					init_green++;
+                    		})
+                	})
+			blue_flag=0;
+		}
+		if(green_flag==1 && blue_flag==0){
+                	console.log("in green");
+                	greenclient.lrange("myimg",0,add,function(err,items){
+                    		if(err) throw err;
+                    		items.forEach(function(item){
+                        		blueclient.lpush('myimg',item);
+					init_blue++;
+                    		})
+                	})
+			green_flag=0;
+            	}
         }
-
         var check= setInterval(trigger, 3*1000);
-
         if(mirror==true){
-            var onChange=setInterval(dup,1*1000);
+		var onChange=setInterval(dup,1*1000);
         }
-
     },
-
     teardown: function () {
         exec('forever stopall', function () {
             console.log("infrastructure shutdown");
             process.exit();
         });
     },
-
 }
 
 infrastructure.setup();
--- Homework/HW4-Data-Migration/blue_green_redis-master/green-www/main.js
+++ Homework/HW4-Data-Migration/blue_green_redis-master/green-www/main.js
@@ -15,25 +15,48 @@ app.get('/switch',function(req,res){
 	res.redirect('/');
 	client.set("switch","1");
 })
+app.get('/set', function(req,res){
+	client.set("key","this message will self-destruct in 10 seconds")
+	client.expire("key",10)
+})
+app.get('/get', function(req,res){
+	res.send(client.get("key", function(err,value){console.log(value)}));
+})
+app.get('/recent', function(req,res){
+	client.ltrim("mylist",0,4)
+	var url_send = ""
+	var urls=client.lrange("mylist",0,4,function(error,items){
+		items.forEach(function(item){
+			url_send=url_send+"\n\t"+item.toString()
+		})
+		res.send(url_send)
+	})
+});
 app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
-	console.log(req.body)
-	console.log(req.files)
+	console.log(req.body) // form fields
+	console.log(req.files) // form files
 	if( req.files.image )
 	{
 		fs.readFile( req.files.image.path, function (err, data) {
 			if (err) throw err;
 			var img = new Buffer(data).toString('base64');
+			console.log(img);
 			client.lpush("myimg",img)
 		});
 	}
 	res.status(204).end()
 }]);
 app.get('/meow', function(req, res) {
-	{
-		client.lpush("myPages",req.url)
-		client.lrange("myimg",0,1,function(err,items){
-			var imagedata=items[0]
-			res.send("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+ 	{
+		var imagedata
+		res.writeHead(200, {'content-type':'text/html'});
+		var imgs=client.lrange("myimg",0,0,function(error,items){
+			items.forEach(function(item){
+				imagedata=item
+				client.lpop("myimg")
+    			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+			})
+ 			res.end();
 		})
 	}
 })
